5df5ee4ec8fa0d8bc6f54d8f1ffb6895ceb65c3e
melkihaythem <melkihaythem@users.noreply.github.com>
2019-03-21 12:54:24 +0100

    schema haythem

---
+++ server/models/RDV.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let RDVSchema = new Schema({
+    Date:{type:Date,
+        required:true},
+    Doctor_avail:{type:Schema.Types.ObjectId,
+                ref:'User',
+                required:true}
+})
+module.exports = mongoose.model('RDV',RDVSchema);
