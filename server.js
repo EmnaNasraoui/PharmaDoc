3b98bd28f1f2df0d75d61ee86c817a7e548650b7
EmnaNasraoui <EmnaNasraoui@users.noreply.github.com>
2019-03-22 15:08:24 +0100

    chat

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ let express = require('express')
 let cors = require('cors')
 let app = express()
 const auth = require('./server/routes/auth')
+const chatBox = require('./server/routes/chatBox')
 app.use(express.json())
 app.use(cors())
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server, {origins: '*:*'});
+
+app.io = io;
 
 let mongoose = require('mongoose')
 let mongoDB = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME
@@ -15,6 +22,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true }).then(
 let db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.use('/auth', auth)
-app.listen(process.env.PORT, function () {
+app.use('/chatBox', chatBox)
+server.listen(process.env.PORT, function () {
     console.log('Express server listening on port ' + process.env.PORT)
   })
