16ec4aae41afbb3d505619d75dce87bc8c1b3161
melkihaythem <melkihaythem@users.noreply.github.com>
2019-03-25 16:05:33 +0100

    haythem

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 var Patient = require('../models/patient');
 var Doctor = require('../models/doctor');
 var RDV = require('../models/RDV');
+var user = require('../models/user')
 let Mongoose = require('mongoose');
 let ObjectId = Mongoose.Types.ObjectId;
 
@@ -15,6 +16,11 @@ router.post('/getRDV/:id_doctor', async (req,res)=>{
     let id = ObjectId(req.params.id_doctor);
     const result = await RDV.create(req.body).catch(err => err)
     const result1 = await Doctor.findByIdAndUpdate({_id:id},{$set:{All_Appointment:result._id}})
+    res.send(result)
+})
+
+router.get('/get_list_doctor', async (req,res)=>{
+        const result = await user.find({}).populate({path:'id_doctor',select:['first_name','last_name']}).catch(err => err)
     res.send(result)
 })
 
