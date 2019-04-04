7550d520f6c3d9057013775b848c381f3c642b07
melkihaythem <melkihaythem@users.noreply.github.com>
2019-04-04 15:30:45 +0200

    haythem

--- server/routes/Pharmacy.js
+++ server/routes/Pharmacy.js
@@ -5,15 +5,18 @@ var Partnership = require('../models/partnership');
 var Product = require('../models/products');
 let Mongoose = require('mongoose');
 let ObjectId = Mongoose.Types.ObjectId ;
+
+
 router.post('/addPharmacy', async (req,res)=>{
     const result = await Pharmacy.create(req.body).catch(err => err)
     res.send(result)
-})
+}) ;
 
 router.post('/editPharmacy/:id', async (req, res) => {
     const result = await Pharmacy.findByIdAndUpdate(req.params.id, { $set: req.body }).catch(err => err)
     res.send(result)
-})
+}) ;
+
 router.post('/ValidatePartnership/:id_pharmacy/:id_doctor/:id_partnership', async (req, res) => {
     let Id_pharmacy = { _id: ObjectId(req.params.id_pharmacy) }
     let Id_doctor = { _id: ObjectId(req.params.id_doctor) }
@@ -24,12 +27,12 @@ router.post('/ValidatePartnership/:id_pharmacy/:id_doctor/:id_partnership', asyn
     const result2 = await Partnership.findByIdAndUpdate(req.params.id_partnership,{$set: { PartnerShip_Status : 2 } }).exec().catch(err => err)
 
     res.send({ msg: result, message: results, aa : result2 })
-})
+}) ;
 
 router.post('/RejectPartnership/:id_partnership', async (req, res) => { 
     const result2 = await Partnership.findByIdAndUpdate(req.params.id_partnership,{$set: { PartnerShip_Status : 3 } }).exec().catch(err => err)
     res.send({message: result2 })
-})
+}) ; 
 
 router.post('/deleteaPartnership/:id_pharmacy/:id_doctor', async (req, res) => {
     let Id_pharmacy = ObjectId(req.params.id_pharmacy)
@@ -37,11 +40,11 @@ router.post('/deleteaPartnership/:id_pharmacy/:id_doctor', async (req, res) => {
     const result = await Pharmacy.updateOne({ $pull: { Pharmacy_Doctor: Id_doctor } }).exec().catch(err => err)
     const results = await Doctor.updateOne({ $pull: { partnership: Id_pharmacy } }).exec().catch(err => err)
     res.send({ msg: result, message: results})
-})
+}) ;  
 
 router.post('/addProduct', async (req,res)=>{
     const result = await Product.create(req.body).catch(err => err)
     res.send(result)
-})
+}) ;
 
-module.exports = router
+module.exports = router 
